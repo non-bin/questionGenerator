68ec171981eb81f927b6fa91a0b4b758d9311cfb
Alice Jacka <jacka.alice@gmail.com>
2019-05-18 16:56:34 +1000

    works now :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,38 @@
+var wordOne     = '';
+var wordTwo     = '';
+var wordOneNew = '';
+var wordTwoNew = '';
+
+// var outWordOne = document.getElementById('word1');
+// var outWordTwo = document.getElementById('word2');
+
 function start() {
+	console.log(wordlist);
 
+	genBothWords();
 }
 
-function wordOneClicked() {
+function genBothWords() {
+	do {
+		wordOneNew = wordlist[0][randomUpTo(wordlist[0].length-1)];
+	} while (wordOne == wordOneNew);
 
+	wordOne = wordOneNew;
+	document.getElementById('word1').innerHTML = wordOne;
+
+	genWordTwo();
 }
 
-function wordTwoClicked() {
+function genWordTwo() {
+	do {
+		wordTwoNew = wordlist[1][wordOne][randomUpTo(wordlist[1][wordOne].length-1)];
+	} while (wordTwo == wordTwoNew);
 
+	wordTwo = wordTwoNew;
+	document.getElementById('word2').innerHTML = wordTwo;
 }
 
-function genWordOne() {
-
-}
-
-function genWordTwo(wordOne) {
-
+function randomUpTo(max) // min and max included
+{
+    return Math.floor(Math.random()*(max+1));
 }
