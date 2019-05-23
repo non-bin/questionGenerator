fde06cda2db26da5ce85da80e889fb758868c2cc
Alice Jacka <jacka.alice@gmail.com>
2019-05-23 19:18:23 +1000

    updated wordlist

--- wordlist.js
+++ wordlist.js
@@ -1,20 +1,46 @@
+// Define the lists of words for the generator
+//
+// First is the list of first words
+// Second is either a list of second words for each first word, or a string to point to another first word to use the list of
+
 const wordlist =
 [
 	[
-		"who",
-		"what",
-		"when",
-		"where",
-		"why",
-		"how"
+		'is',
+		'are',
+		'was',
+		'did',
+		'would',
+		'might',
+		'will',
+		'can',
+		'could',
+		'how',
+		'when',
+		'where',
+		'why',
+		'who',
+		'what',
+		'which',
 	],
 
 	{
-		"who": ["is", "went", "didn't"],
-		"what": ["was", "stole", "entered"],
-		"when": ["did", "was", "is"],
-		"where": ["did", "was", "is"],
-		"why": ["did", "was", "is"],
-		"how": ["did", "was", "is"]
+		'is'   : ['he', 'she', 'it'],
+		'did'  : ['they', 'their', 'we', 'the', 'any', 'my', 'his', 'her'],
+		'how'  : ['is', 'are', 'was', 'did', 'would', 'might', 'will', 'can', 'could', 'were'],
+		'which': ['one', 'friend', 'year', 'day', 'goat 🐐'],
+		'was'  : 'is',
+		'were' : 'are',
+		'are'  : 'did',
+		'would': 'did',
+		'might': 'did',
+		'will' : 'did',
+		'can'  : 'did',
+		'could': 'did',
+		'when' : 'how',
+		'where': 'how',
+		'why'  : 'how',
+		'who'  : 'how',
+		'what' : 'how',
 	}
 ]
